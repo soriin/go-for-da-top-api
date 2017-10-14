820e4052f384b9fdd665803c91f137c0f3ed30bc
Elliot Fisher <mr.efisher@gmail.com>
2017-10-13 20:00:54 -0400

    refactored login controller to fit with style

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,33 +1,31 @@
-const passport = require('passport');
-const User = require('../models/user');
+const passport = require('passport')
+const User = require('../models/user')
+const logger = require('../logging/logger')
 
 const facebookHandler = [passport.authenticate('facebook')];
 
 const facebookReturnHandler = [
   passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function (req, res) {
+  async function facebookReturnFunc (req, res) {
     res.cookie('gfdt_access_token_id', req.user.accessToken)
     res.cookie('gfdt_access_token_userid', req.user.profile.id)
     res.cookie('gfdt_access_token_user', req.user.profile)
 
-    User.findOne({ facebookId: req.user.profile.id })
-      .then((dbUser) => {
-        if (dbUser) {
-          dbUser.accessToken = req.user.accessToken;
-          console.log('updated user', dbUser)
-          return dbUser.save();
-        } else {
-          let newUser = new User();
-          newUser.accessToken = req.user.accessToken;
-          newUser.facebookId = req.user.profile.id;
-          newUser.realName = req.user.profile.displayName;
-          console.log('added new user', req.user)
-          return newUser.save()
-        }
-      })
-      .then(() => {
-        res.redirect(process.env.clientRootUrl);
-      })
+    const dbUser = await User.findOne({ facebookId: req.user.profile.id }).exec()
+
+    if (dbUser) {
+      dbUser.accessToken = req.user.accessToken;
+      logger.info('updated user', dbUser)
+      await dbUser.save();
+    } else {
+      let newUser = new User();
+      newUser.accessToken = req.user.accessToken;
+      newUser.facebookId = req.user.profile.id;
+      newUser.realName = req.user.profile.displayName;
+      logger.info('added new user', req.user)
+      await newUser.save()
+    }
+    res.redirect(process.env.clientRootUrl);
   }
 ]
 
