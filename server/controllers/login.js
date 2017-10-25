71d66ebb281338ffbad8c0192435d76b69652e16
Elliot Fisher <mr.efisher@gmail.com>
2017-10-25 19:53:45 -0400

    limiting cookie usage

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -8,8 +8,6 @@ const facebookReturnHandler = [
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   async function facebookReturnFunc (req, res) {
     res.cookie('gfdt_access_token_id', req.user.accessToken)
-    res.cookie('gfdt_access_token_userid', req.user.profile.id)
-    res.cookie('gfdt_access_token_user', req.user.profile)
 
     const dbUser = await User.findOne({ facebookId: req.user.profile.id }).exec()
 
