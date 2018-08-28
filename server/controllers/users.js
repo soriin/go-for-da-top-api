c8824435a4d56172a562b52131663ace0a3c3640
Elliot Fisher <mr.efisher@gmail.com>
2018-08-28 00:15:17 -0400

    user data sanitizing

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,7 +4,12 @@ const getCurrentUserHandler = [
   (req, res) => {
     const {user, token} = res.locals
     logger.info(user, token)
-    res.send(user)
+    res.send({
+      _id: user._id,
+      realName: user.realName,
+      displayName: user.displayName,
+      isAdmin: user.isAdmin
+    })
   }
 ]
 
