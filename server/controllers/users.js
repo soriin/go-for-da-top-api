4a5e6fe9a5569fd2d265e4023dc84ba1e940b92a
Elliot Fisher <mr.efisher@gmail.com>
2018-09-09 15:45:08 -0400

    GET user/:id - endpoint added

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,18 +1,44 @@
 const logger = require('../logging/logger')
+const User = require('../models/user')
+
+const massageUser = (dbUser) => {
+  return {
+    _id: dbUser._id,
+      realName: dbUser.realName,
+      displayName: dbUser.displayName,
+      isAdmin: dbUser.isAdmin
+  }
+}
 
 const getCurrentUserHandler = [
   (req, res) => {
     const {user, token} = res.locals
     logger.info(user, token)
-    res.send({
-      _id: user._id,
-      realName: user.realName,
-      displayName: user.displayName,
-      isAdmin: user.isAdmin
-    })
+    res.send(massageUser(user))
+  }
+]
+
+const getUserHandler = [
+  async (req, res) => {
+    try {
+      const userId = req.params.id
+      logger.info(`getting user data for ${userId}`)
+      const user = await User
+        .findOne({
+          _id: userId
+        })
+        .lean()
+        .exec()
+      
+      res.send(massageUser(user))
+    } catch (e) {
+      logger.error(e.stack)
+      res.status(500).send({ error: 'unable to retrieve user data' })
+    }
   }
 ]
 
 module.exports = {
-  getCurrentUserHandler
+  getCurrentUserHandler,
+  getUserHandler
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const userController = require('../controllers/users')
 
 router.get('/me', ...userController.getCurrentUserHandler)
+router.get('/:id', ...userController.getUserHandler)
 
 module.exports = router
