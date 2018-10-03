891269a4b6b5e82a26fa4297033bb202e89e431e
Elliot Fisher <mr.efisher@gmail.com>
2018-10-02 23:34:27 -0400

    working edit user endpoint

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,18 +1,20 @@
 const logger = require('../logging/logger')
 const User = require('../models/user')
+const sanitizeSvc = require('../services/sanitizeService')
 
 const massageUser = (dbUser) => {
   return {
     _id: dbUser._id,
-      realName: dbUser.realName,
-      displayName: dbUser.displayName,
-      isAdmin: dbUser.isAdmin
+    realName: dbUser.realName,
+    displayName: dbUser.displayName,
+    isAdmin: dbUser.isAdmin,
+    email: dbUser.email
   }
 }
 
 const getCurrentUserHandler = [
   (req, res) => {
-    const {user, token} = res.locals
+    const { user, token } = res.locals
     logger.info(user, token)
     res.send(massageUser(user))
   }
@@ -29,7 +31,34 @@ const getUserHandler = [
         })
         .lean()
         .exec()
-      
+
+      res.send(massageUser(user))
+    } catch (e) {
+      logger.error(e.stack)
+      res.status(500).send({ error: 'unable to retrieve user data' })
+    }
+  }
+]
+
+const updateUserHandler = [
+  async (req, res) => {
+    try {
+      const userId = req.params.id
+      logger.info(`getting user data for ${userId}`, req.body)
+
+      const validProperties = [
+        'displayName',
+        'realName',
+        'email'
+      ]
+      const cleanedData = sanitizeSvc.sanitize(req.body, validProperties)
+      const user = await User
+        .findOneAndUpdate({
+          _id: userId
+        },
+          cleanedData,
+          { new: true }).exec()
+
       res.send(massageUser(user))
     } catch (e) {
       logger.error(e.stack)
@@ -40,5 +69,6 @@ const getUserHandler = [
 
 module.exports = {
   getCurrentUserHandler,
-  getUserHandler
+  getUserHandler,
+  updateUserHandler
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,4 +4,6 @@ const userController = require('../controllers/users')
 router.get('/me', ...userController.getCurrentUserHandler)
 router.get('/:id', ...userController.getUserHandler)
 
+router.put('/:id', ...userController.updateUserHandler)
+
 module.exports = router
